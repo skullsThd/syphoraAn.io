1cdf1ade9519497d2a0ae002b6a02c4abeabd731
JKR_Anto08 <91921765+AntonioGervasio@users.noreply.github.com>
2023-05-19 23:05:57 +0200

    Update login.php
    Signed-off-by: JKR_Anto08 <91921765+AntonioGervasio@users.noreply.github.com>

--- login.php
+++ login.php
@@ -1,21 +1,83 @@
 <?php
-// Verifica se sono stati inviati i dati di login
-if (isset($_POST['username']) && isset($_POST['password'])) {
-  $username = $_POST['Nome Utente'];
-  $password = $_POST['Parola Ordine];
+// Controlla se l'utente ha effettuato l'accesso
+session_start();
 
-  // Esegui qui il codice per il controllo delle credenziali
-  // Ad esempio, puoi confrontare i valori con un database di utenti
-
-  // Esempio di controllo delle credenziali hardcoded per "admin" e "password"
-  if ($username === 'admin' && $password === 'password') {
-    // Login riuscito, reindirizza a una pagina successiva
-    header('Location: dashboard.php');
-    exit();
-  } else {
-    // Login fallito, reindirizza alla pagina di login con un messaggio di errore
-    header('Location: login.php?error=1');
-    exit();
-  }
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
+  // Se l'utente ha già effettuato l'accesso, reindirizzalo alla dashboard
+  header("Location: dashboard.php");
+  exit();
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="it-it">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Syphora Antivirus</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="shortcut icon" type="image/png" href="Logo.png">
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="style.css">
+  <style>
+    /* Stili personalizzati */
+  </style>
+</head>
+
+<body monica-locale="it" monica-version="1.6.2" monica-id="ofpnmcalabcbjgholdjcjblkibolbppb">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#features">Caratteristiche</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#pricing">Prezzi</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">Contattami</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="login.php">Accedi</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="build.html">Build</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <header id="home">
+    <!-- Resto del codice HTML -->
+  </header>
+
+  <section id="features">
+    <!-- Resto del codice HTML -->
+  </section>
+
+  <section id="pricing">
+    <!-- Resto del codice HTML -->
+  </section>
+
+  <section id="contact">
+    <!-- Resto del codice HTML -->
+  </section>
+
+  <footer>
+    <!-- Resto del codice HTML -->
+  </footer>
+
+  <!-- jQuery and Bootstrap Bundle (includes Popper) -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4y4gSlBnj9D8FbS2Mhxh" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <!-- Custom JavaScript -->
+  <script src="script.js"></script>
+</body>
+</html>
