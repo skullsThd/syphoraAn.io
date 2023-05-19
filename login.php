1f4f9a9d3ad8cff9ed87205d95ff159af9f4ff0d
skullsThd <104901809+skullsThd@users.noreply.github.com>
2023-05-19 22:39:07 +0200

    Create login.php
    Signed-off-by: skullsThd <104901809+skullsThd@users.noreply.github.com>

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+// Verifica se sono stati inviati i dati di login
+if (isset($_POST['username']) && isset($_POST['password'])) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  // Esegui qui il codice per il controllo delle credenziali
+  // Ad esempio, puoi confrontare i valori con un database di utenti
+
+  // Esempio di controllo delle credenziali hardcoded per "admin" e "password"
+  if ($username === 'admin' && $password === 'password') {
+    // Login riuscito, reindirizza a una pagina successiva
+    header('Location: dashboard.php');
+    exit();
+  } else {
+    // Login fallito, reindirizza alla pagina di login con un messaggio di errore
+    header('Location: login.php?error=1');
+    exit();
+  }
+}
+?>
